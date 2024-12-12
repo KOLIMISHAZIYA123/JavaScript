69d366729534cf20b1897c905fc3733fe6073503
KOLIMISHAZIYA123 <165468966+KOLIMISHAZIYA123@users.noreply.github.com>
2024-12-12 12:03:28 +0000

    operations

---
+++ Basics/Operations.js
@@ -0,0 +1,21 @@
+// let score="33abc"
+let score=null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) //NaN 
+//"33"=>33
+//"33abc" => NaN
+//true => 1;false => 0
+
+let isLoggedIn="khan"
+let booleanISLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanISLoggedIn)
+
+let number=33
+let stringNumber=String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber)
